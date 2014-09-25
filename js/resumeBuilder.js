a57b8154629fd98bcadebd48dc692146e3bccff5
Sergio Barrera <sdbeng@gmail.com>
2014-09-25 11:24:50 -0700

    Pull data from resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,20 @@
+var name = "Dani";
+//$("#main").append(name);
+//quiz1
+// var awesomeThoughts = "I am Dani and I am AWESOME!";
+// console.log(awesomeThoughts);
+
+//quiz2: trying out [string].replace([old],[new])
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// $("#main").append(funThoughts);
+
+//quiz 3
+var formattedName = HTMLheaderName.replace("%data%",name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+//In this way will place the role first then will prepend the name afterwards
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
